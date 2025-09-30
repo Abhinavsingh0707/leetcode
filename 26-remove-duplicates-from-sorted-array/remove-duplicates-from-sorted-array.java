aea26a8eba35333629001f4dc42aaa3cbcdfbe1e
Abhinav Singh <abhinavsinghinright@gmail.com>
2025-10-01 00:54:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (24.46%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
       // 1 2 3 3 3 4 - > 1 2 3 4 8 9
       //   j
       int k = 1;
-      for(int j = 1;j < nums.length; j++){
+      for(int j = 1;j < nums.length; j++){  // TC -> O(n)
         if(nums[j] != nums[j-1]){
             nums[k] = nums[j];
             k = k+1;
