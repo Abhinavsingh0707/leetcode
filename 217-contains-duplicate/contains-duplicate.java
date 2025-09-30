907c94ad6ae58d17571e569366b7ae3b571a6fe0
Abhinav Singh <abhinavsinghinright@gmail.com>
2025-10-01 03:38:01 +0530

    Time: 14 ms (61.89%) | Memory: 58.8 MB (31.25%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+    
+        /*Arrays.sort(nums);
         
         for(int i=1 ; i<nums.length; i++){
               if(nums[i] == nums[i-1]){
@@ -9,7 +9,18 @@ class Solution {
 
 
         }
-      
+      */
+      public boolean containsDuplicate(int[] nums) {
+      Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+
+                 }
+            set.add(num);
+         }
         return false;
+         
     }
+    
 }
